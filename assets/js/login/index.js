3fcb2b8cb3b4d387cfedd1e901af2fb208fd17bf
rabota <bossic_23@inbox.ru>
2018-04-20 09:15:55 +0500

    17.04.2018 +login +button

--- assets/js/login/index.js
+++ assets/js/login/index.js
@@ -1,12 +1,41 @@
-  $(document).ready(function(){
-        $('.log-btn').click(function(){
-            $('.log-status').addClass('wrong-entry');
-           $('.alert').fadeIn(500);
-           setTimeout( "$('.alert').fadeOut(1500);",3000 );
-        });
-        $('.form-control').keypress(function(){
-            $('.log-status').removeClass('wrong-entry');
-        });
 
-    });
 
+$(".log-btn").on('click', function () {
+      let inputs = $(this).parent().find("input");
+
+      struct = {
+          "login":inputs[0].value,
+          "password":inputs[1].value
+      };
+
+      $.ajax({
+          url     : "/login",
+          type    : "POST",
+          contentType: "application/json",
+          data    : JSON.stringify(struct),
+          success : function(data){
+              if (data == null) {
+                  $(".alert").css({ opacity: 1 });
+                  location.href = "/map"
+              } else {
+
+                  $('.log-btn').click(function(){
+                      $('.log-status').addClass('wrong-entry');
+                      $('.alert').fadeIn(500);
+                      setTimeout( "$('.alert').fadeOut(1500);",3000 );
+                  });
+
+                  $('.form-control').keypress(function(){
+                      $('.log-status').removeClass('wrong-entry');
+                  });
+              }
+          }
+      });
+  });
+
+
+$("#Passwod").keyup(function(event){
+  if(event.keyCode == 13){
+      $(".log-btn").click();
+  }
+});
